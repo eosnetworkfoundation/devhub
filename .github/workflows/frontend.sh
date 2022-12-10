253ca200f2a1f7e5f85a8ecfddd2974c2a54c4b8
Zach Butler <zach.butler@eosnetwork.com>
2022-12-10 01:39:17 -0500

    Start writing DevHub frontend CICD build steps

---
+++ .github/workflows/frontend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eo pipefail
+
+function ee()
+{
+    echo "$ $*"
+    eval "$@"
+}
+
+# print debugging code
+ee node --version
+ee yarn --version
+if [[ ! -z "$BACKEND_API" ]]; then
+    ee 'printf "$BACKEND_API" | wc -c'
+else
+    printf '\e[93mWARNING: BACKEND_API is not defined!\e[0m\n'
+fi
+# init
+ee pushd frontend
+ee yarn --frozen-lockfile
+# generate static site
+ee yarn generate
+# pack dist folder
+ee tar -cvf dist.tar.gz dist/*
+echo 'Done! - frontend.sh'
