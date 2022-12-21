ced26884a44e922c69ddf57836908717ba531751
Zach Butler <zach.butler@eosnetwork.com>
2022-12-20 22:39:14 -0500

    Clarify print statement about newest artifact

--- .github/workflows/frontend-publish.sh
+++ .github/workflows/frontend-publish.sh
@@ -11,7 +11,7 @@ ee 'aws --version'
 echo 'Finding newest matrix artifact.'
 ee 'ls -la'
 export DIST_DL_FOLDER="$(find . -maxdepth '1' -name 'dist*' -type 'd' | sort -r | head -n '1')"
-echo "Identified \"$DIST_DL_FOLDER\" as the matrix build output from the most recent nodeJS version."
+echo "Identified \"$DIST_DL_FOLDER\" as the matrix build artifact from the most recent nodeJS version."
 ee "pushd '$DIST_DL_FOLDER'"
 ee 'tar -xzf dist.tar.gz'
 ee 'pushd dist'
