5b53ac4869cd8e67da707fbd0c0d6a78032db94c
Zach Butler <zach.butler@eosnetwork.com>
2022-12-20 22:18:39 -0500

    Start writing AWS publish code, with some debug code

---
+++ .github/workflows/frontend-publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eo pipefail
+
+function ee()
+{
+    echo "$ $*"
+    eval "$@"
+}
+
+echo 'Finding newest matrix artifact.'
+ee 'ls -la'
+export DIST_DL_FOLDER="$(ls -1 | sort -fr | head -1)"
+echo "Identified \"./$DIST_DL_FOLDER\" at the matrix build output from the most recent nodeJS version."
+ee "pushd '$DIST_DL_FOLDER'"
+ee 'tar -xzf dist.tar.gz'
+ee 'pushd dist'
+ee 'ls -la'
+ee 'aws --version'
+echo 'Done! - frontend-publish.sh'
