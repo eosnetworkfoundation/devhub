9951bd0c52d6f518532019464ee2f5c36a3db370
Zach Butler <zach.butler@eosnetwork.com>
2022-12-20 22:37:34 -0500

    Remove a debug statement

--- .github/workflows/frontend-publish.sh
+++ .github/workflows/frontend-publish.sh
@@ -12,7 +12,6 @@ ee 'ls -la'
 export DIST_DL_FOLDER="$(find . -maxdepth '1' -name 'dist*' -type 'd' | sort -r | head -n '1')"
 echo "Identified \"$DIST_DL_FOLDER\" as the matrix build output from the most recent nodeJS version."
 ee "pushd '$DIST_DL_FOLDER'"
-ee 'ls -la'
 ee 'tar -xzf dist.tar.gz'
 ee 'pushd dist'
 ee 'ls -la'
