2e9c249a076ab335854439b0eaa066af2909aef4
Zach Butler <zach.butler@eosnetwork.com>
2022-12-21 03:24:52 -0500

    Fix tag comparison

--- .github/workflows/frontend-aws-context.sh
+++ .github/workflows/frontend-aws-context.sh
@@ -3,10 +3,10 @@ set -eo pipefail
 if [[ "$GITHUB_REF_TYPE" == 'tag' ]]; then
     echo "Found git $GITHUB_REF_TYPE \"$GITHUB_REF_NAME,\" attempting a production deployment."
     export GIT_TAG="$(git --no-pager tag --points-at HEAD)"
-    export FRONTEND_VERSION="$(cat frontend/package.json | jq '.version')"
+    export FRONTEND_VERSION="v$(cat frontend/package.json | jq '.version')"
     if [[ "v$FRONTEND_VERSION" != "$GIT_TAG" || "$GIT_TAG" != "$GITHUB_REF_NAME" ]]; then
         echo '::error:: Frontend package.json version string does not match the git tag!'
-        echo "FRONTEND_VERSION='v$FRONTEND_VERSION'"
+        echo "FRONTEND_VERSION='$FRONTEND_VERSION'"
         echo "GITHUB_REF_NAME='$GITHUB_REF_NAME'"
         echo "GIT_TAG='$GIT_TAG'"
         cat frontend/package.json | jq '.'
