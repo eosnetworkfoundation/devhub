a7727d27c121681ab0e18e5603f7252515887601
Zach Butler <zach.butler@eosnetwork.com>
2022-12-21 00:24:52 -0500

    Pack build metadata into website distribution to make it easier to see what version of the site CDN edge nodes are serving

--- .github/workflows/frontend-publish.sh
+++ .github/workflows/frontend-publish.sh
@@ -15,7 +15,30 @@ echo "Identified \"$DIST_DL_FOLDER\" as the matrix build artifact from the most 
 ee "pushd '$DIST_DL_FOLDER'"
 ee 'tar -xzf dist.tar.gz'
 ee 'pushd dist'
-ee cp ../../frontend/package.json .
+echo 'Packing website metadata into distribution.'
+cat ../../frontend/package.json . | jq -c \
+    --arg actor "$GITHUB_ACTOR" \
+    --arg branch "$(git branch --show-current)" \
+    --arg build "$GITHUB_RUN_NUMBER" \
+    --arg build_id "$GITHUB_RUN_ID" \
+    --arg commit "$(git rev-parse HEAD)" \
+    --arg ref_type "$GITHUB_REF_TYPE" \
+    --arg repo "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" \
+    --arg tag "$(git --no-pager tag --points-at HEAD)" \
+    --arg triggering_actor "$GITHUB_TRIGGERING_ACTOR" \
+    '.git += {
+        $actor,
+        branch: ($branch | if . == "" then null else . end),
+        $build,
+        $build_id,
+        build_url: ($repo + "/actions/runs/" + $build_id),
+        $commit,
+        $ref_type,
+        $repo,
+        tag: ($tag | if . == "" then null else . end),
+        $triggering_actor
+    }'
+ee 'cat package.json | jq .git'
 ee 'ls -la'
 ee 'aws s3 sync "." "s3://$S3_BUCKET" --delete --dryrun'
 echo 'Done! - frontend-publish.sh'
