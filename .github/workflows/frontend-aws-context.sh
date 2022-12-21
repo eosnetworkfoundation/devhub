f77d81dd774557a8d87674d7ff4f43343ed3ac15
Zach Butler <zach.butler@eosnetwork.com>
2022-12-21 03:29:32 -0500

    Remove quotes from package version

--- .github/workflows/frontend-aws-context.sh
+++ .github/workflows/frontend-aws-context.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 if [[ "$GITHUB_REF_TYPE" == 'tag' ]]; then
     echo "Found git $GITHUB_REF_TYPE \"$GITHUB_REF_NAME,\" attempting a production deployment."
     export GIT_TAG="$(git --no-pager tag --points-at HEAD)"
-    export FRONTEND_VERSION="v$(cat frontend/package.json | jq '.version')"
+    export FRONTEND_VERSION="v$(cat frontend/package.json | jq -r '.version')"
     if [[ "v$FRONTEND_VERSION" != "$GIT_TAG" || "$GIT_TAG" != "$GITHUB_REF_NAME" ]]; then
         echo '::error:: Frontend package.json version string does not match the git tag!'
         echo "FRONTEND_VERSION='$FRONTEND_VERSION'"
