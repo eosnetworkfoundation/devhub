2377849f756304af7e0259a62ad36c9c36d9a78c
Zach Butler <zach.butler@eosnetwork.com>
2022-12-21 03:32:11 -0500

    Remove spurious 'v'

--- .github/workflows/frontend-aws-context.sh
+++ .github/workflows/frontend-aws-context.sh
@@ -4,7 +4,7 @@ if [[ "$GITHUB_REF_TYPE" == 'tag' ]]; then
     echo "Found git $GITHUB_REF_TYPE \"$GITHUB_REF_NAME,\" attempting a production deployment."
     export GIT_TAG="$(git --no-pager tag --points-at HEAD)"
     export FRONTEND_VERSION="v$(cat frontend/package.json | jq -r '.version')"
-    if [[ "v$FRONTEND_VERSION" != "$GIT_TAG" || "$GIT_TAG" != "$GITHUB_REF_NAME" ]]; then
+    if [[ "$FRONTEND_VERSION" != "$GIT_TAG" || "$GIT_TAG" != "$GITHUB_REF_NAME" ]]; then
         echo '::error:: Frontend package.json version string does not match the git tag!'
         echo "FRONTEND_VERSION='$FRONTEND_VERSION'"
         echo "GITHUB_REF_NAME='$GITHUB_REF_NAME'"
