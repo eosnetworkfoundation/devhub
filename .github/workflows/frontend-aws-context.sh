e73283e19c8a4961e43dd7c80d6494a79537f3b9
Zach Butler <zach.butler@eosnetwork.com>
2022-12-22 03:55:59 -0500

    Add GitHub Actions notices for deployment type

--- .github/workflows/frontend-aws-context.sh
+++ .github/workflows/frontend-aws-context.sh
@@ -12,10 +12,12 @@ if [[ "$GITHUB_REF_TYPE" == 'tag' ]]; then
         cat frontend/package.json | jq '.'
         exit 10
     fi
+    echo '::notice title=Deploying to Production::This build will attempt to deploy to production. This is the real deal!'
     echo '::set-output name=dry-run::false'
     echo "::set-output name=role-arn::$DEVHUB_FRONTEND_PROD_IAM_ARN"
 else
     echo "Found git $GITHUB_REF_TYPE \"$GITHUB_REF_NAME,\" performing a dry-run."
+    echo '::notice title=Dry Run::This build is performing a dry run. A dry run attemps to verify everything is good to go without actually changing anything.'
     echo '::set-output name=dry-run::true'
     echo "::set-output name=role-arn::$DEVHUB_FRONTEND_RO_IAM_ARN"
 fi
