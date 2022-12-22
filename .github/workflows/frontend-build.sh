a0d7443eb9450c3a2b24bbdbc91ececf4c81f0be
Zach Butler <zach.butler@eosnetwork.com>
2022-12-22 01:55:31 -0500

    Add a warning to the GitHub Actions workflow if we cannot connect to the backend API

--- .github/workflows/frontend-build.sh
+++ .github/workflows/frontend-build.sh
@@ -7,14 +7,21 @@ function ee()
     eval "$@"
 }
 
+exec 9>&1 # enable tee to write to STDOUT as a file
 # print debugging code
 ee node --version
 ee yarn --version
 ee npm --version
 if [[ ! -z "$DEVHUB_BACKEND_API" ]]; then
     ee 'printf "$DEVHUB_BACKEND_API" | wc -c'
-    ee 'curl -fsSL "$DEVHUB_BACKEND_API/test"'
+    export BACKEND_UP="$(ee 'curl -fsSL "$DEVHUB_BACKEND_API/test"' | tee >(cat - >&9))"
     echo
+    if [[ "$BACKEND_UP" == 'true' ]]; then
+        echo 'DevHub backend API up!'
+    else
+        printf '\e[93mWARNING: Failed to connect to DevHub backend API!\e[0m\n'
+        echo '::warning title=Failed to Connect to DevHub Backend API::Failed to connect to DevHub backend API!'
+    fi
 else
     printf '\e[93mWARNING: DEVHUB_BACKEND_API is not defined!\e[0m\n'
     echo '::warning title=DevHub Backend API Endpoint Missing::DEVHUB_BACKEND_API is not defined!'
